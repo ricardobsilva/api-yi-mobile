36a7ce8f6508453749c04d4501a422973be11465
Ricardo Silva <ricardo@safetysystemtechnology.com.br>
2019-02-10 01:29:45 -0300

    Create seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create(name: 'João', email: 'joao@gmail.com', password: '123123123')
+
+User.create(name: 'Jose', email: 'jose@gmail.com', password: '123123123')
+
+Event.create( name:'Rock in Rio' ,description: 'Festival de musica idealizado por Roberto Medina', lat: '-22.9035' , long: '-43.2096')
+
+Event.create( name:'LolaPalooza' ,description: 'Festival anual de música alternativa', lat: '-23.5489', long: '-46.6388')
+
+Event.create( name:'Carnaval da Bahia 2019' ,description: 'Uma das principais festas da cultura baiana', lat: '-12,9722' ,long: '-38,5014')
